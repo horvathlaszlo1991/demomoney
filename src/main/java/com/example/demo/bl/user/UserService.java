c4dc15f67834b86ee740bfe53735b8d71d83a4d0
Laszlo Horvath <horvathlaszlo1991@gmail.com>
2019-02-14 12:18:01 +0100

    added methods to userservice

--- src/main/java/com/example/demo/bl/user/UserService.java
+++ src/main/java/com/example/demo/bl/user/UserService.java
@@ -1,7 +1,12 @@
 package com.example.demo.bl.user;
 
 import com.example.demo.db.user.UserDao;
+import com.example.demo.model.Response;
+import com.example.demo.model.User;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -10,6 +15,34 @@ public class UserService {
 
     public UserService(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+    public Response createUser(User user) {
+        return userDao.createUser(user);
+    }
+
+    public List<User> listAllUsers() {
+        return userDao.listAllUsers();
+    }
+
+    public List<User> listActiveUsers() {
+        return userDao.listActiveUsers();
+    }
+
+    public Response deleteUserById(long id) {
+        return userDao.deleteUserById(id);
+    }
+
+    public Response updateUser(User user) {
+        return userDao.updateUser(user);
+    }
+
+    public Optional<User> findUserById(long id) {
+        return userDao.findUserById(id);
+    }
+
+    public Optional<User> findUserByEmail(String email) {
+        return userDao.findUserByEmail(email);
     }
 
 
