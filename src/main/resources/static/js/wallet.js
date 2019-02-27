c75dcb90745c500a71d3dc18f7f39208e3dc4c89
Laszlo Horvath <horvathlaszlo1991@gmail.com>
2019-02-27 15:35:26 +0100

    some more js in wallet

--- src/main/resources/static/js/wallet.js
+++ src/main/resources/static/js/wallet.js
@@ -3,18 +3,22 @@ createForm();
 
 function createForm() {
     let form = document.getElementById("form");
+    let br = document.createElement("br");
     let pcash = document.createElement("p");
     pcash.innerHTML = "Enter cash amount";
     let pcard = document.createElement("p");
     pcard.innerHTML = "Enter card amount";
-    let cashForm = document.createElement("input");
-    cashForm.setAttribute("type", "text");
-    cashForm.setAttribute("name", "cash");
-    let cardForm = document.createElement("input");
-    cardForm.setAttribute("type", "text");
-    cardForm.setAttribute("name", "card");
     form.appendChild(pcash);
-    form.appendChild(cashForm);
+    createInput(form, "text", "cash");
     form.appendChild(pcard);
-    form.appendChild(cardForm);
+    createInput(form, "text", "card");
+    form.appendChild(br);
+    createInput(form, "submit", "OK");
+}
+
+function createInput(place, type, name) {
+    let input = document.createElement("input");
+    input.setAttribute("type", type);
+    input.setAttribute("name", name);
+    place.appendChild(input);
 }
